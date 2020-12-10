8f53d2d4c597c7d21f73270fbb5705fff49f4cdc
Johannes Norrbacka <johannes.norrbacka@youcal.com>
2020-12-11 00:38:23 +0100

    really shitty solution for part 2, tried to clean the madness up, but it works, so I guess it is okay

--- lib/day10/day10.ex
+++ lib/day10/day10.ex
@@ -13,18 +13,9 @@ defmodule Aoc2020.Day10 do
       inputs
       |> Enum.find(fn input -> input - current_jolt <= max_steps end)
 
-    IO.inspect(%{
-      :current_jolt => current_jolt,
-      :next_adapter => next_adapter,
-      :one_diff => one_diff,
-      :two_diff => two_diff,
-      :three_diff => three_diff
-    })
-
     cond do
       next_adapter == nil ->
         final_adapter = current_jolt + 3
-
         %{
           :final_adapter => final_adapter,
           :one_diff => one_diff,
@@ -70,8 +61,83 @@ defmodule Aoc2020.Day10 do
       "./lib/day10/input.txt"
       |> read()
 
-    x = IO.inspect traverse_and_count(input, 0, 0, 0, 0, 3)
+    x = IO.inspect(traverse_and_count(input, 0, 0, 0, 0, 3))
     x.one_diff * x.three_diff
   end
 
+  def get_paths_rec(array, current, result) do
+    case Enum.any?(array) do
+      false ->
+        result
+
+      true ->
+        next = array |> Enum.at(0)
+
+        case next - current < 3 do
+          true ->
+            result = (result || []) |> Enum.concat([next])
+            [_ | array] = array
+            get_paths_rec(array, next, result)
+
+          false ->
+            result
+        end
+    end
+  end
+
+  def get_paths_rec(array, grps) do
+    [h | _t] = array
+    grp = get_paths_rec(array, h, [])
+    grps = grps |> Enum.concat([grp])
+    new_array = Enum.slice(array, Enum.count(grp)..Enum.count(array))
+
+    case Enum.empty?(new_array) do
+      true -> grps
+      false -> get_paths_rec(new_array, grps)
+    end
+  end
+
+  def count_valid_paths(group) do
+    Comb.subsets(group)
+    |> Enum.filter(fn x -> Enum.empty?(x) == false end)
+    |> Enum.filter(fn x ->
+      Enum.any?(x, fn z -> z == List.first(group) end) and
+        Enum.any?(x, fn z -> z == List.last(group) end)
+    end)
+    |> Enum.filter(fn x ->
+      Enum.chunk_every(x, 2, 1, :discard)
+      |> Enum.all?(fn a ->
+        n = (Enum.at(a, 1) - Enum.at(a, 0)) |> Kernel.abs()
+        n <= 3
+      end)
+    end)
+    |> Enum.count()
+  end
+
+  def part2(input) do
+    get_paths_rec([0] ++ input, [])
+    |> Enum.map(&count_valid_paths/1)
+    |> Enum.reduce(fn a, b -> a * b end)
+  end
+
+  # Aoc2020.Day10.task2_small_test
+  def task2_small_test do
+    "./lib/day10/small_test.txt"
+    |> read()
+    |> part2
+  end
+
+  # Aoc2020.Day10.task2_larger_test
+  def task2_larger_test do
+    "./lib/day10/larger_test.txt"
+    |> read()
+    |> part2
+  end
+
+  # Aoc2020.Day10.task2
+  def task2 do
+    "./lib/day10/input.txt"
+    |> read()
+    |> part2
+  end
 end
--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Aoc2020Elixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:comb, git: "https://github.com/tallakt/comb.git", tag: "master"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
