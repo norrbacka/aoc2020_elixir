a08dcdfd0fd15743164b056b89a9e804de8ce44d
Johannes Norrbacka <johannes.norrbacka@rajosoftware.se>
2020-12-16 00:53:21 +0100

    one day behind, but finally finished part 2 of day 14

--- lib/day14/day14.ex
+++ lib/day14/day14.ex
@@ -93,4 +93,85 @@ defmodule Aoc2020.Day14 do
     end)
     |> Enum.sum()
   end
+
+  def replace_on_x(mask, combo) do
+    case Enum.empty?(combo) do
+      true ->
+        mask
+      false ->
+        [h | t] = combo
+        replace_on_x(String.replace(mask, "X", h, global: false), t)
+    end
+  end
+
+  def get_combinations(mask) do
+
+    mask_list = mask
+    |> String.graphemes()
+
+    xs_count = mask_list
+    |> Enum.filter(fn x -> x == "X" end)
+    |> Enum.count
+
+    combinations_count = :math.pow(2, xs_count) |> round
+
+    (0..combinations_count-1)
+    |> Enum.to_list()
+    |> Enum.map(fn i ->
+      mask_combo =
+        :io_lib.format("~#{xs_count}.2.0B", [i])
+        |> List.to_string()
+        |> String.graphemes()
+      replace_on_x(mask, mask_combo)
+    end)
+  end
+
+  def apply_mask(mask, number) do
+    to_binary_string(number)
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.map(fn {number, index} ->
+      case Enum.at(mask, index) do
+        # If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1
+        "1" -> 1
+        # If the bitmask bit is 0, the corresponding memory address bit is unchanged
+        "0" -> number
+        "X" -> "X"
+      end
+    end)
+    |> Enum.join("")
+  end
+
+  def apply_data_to_memory(data, memory) do
+    case Enum.empty?(data) do
+      true -> memory
+      false ->
+        [datapoint | rest] = data
+        new_values = datapoint.instructions
+        |> Enum.flat_map(fn i ->
+          applied_mask = apply_mask(datapoint.mask |> String.graphemes, i.mem)
+          combos = get_combinations(applied_mask)
+          combos |> Enum.map(fn c ->
+            %{
+              :key => c,
+              :value => i.value
+            }
+          end)
+         end)
+        apply_data_to_memory(rest, memory ++ new_values)
+    end
+  end
+
+  # Aoc2020.Day14.task2
+  def task2() do
+    data = "./lib/day14/input.txt"
+    |> read()
+
+    x = apply_data_to_memory(data, []) |> Enum.reverse()
+    keys = Enum.map(x, fn d -> d.key end) |> Enum.uniq()
+    values = Enum.map(keys, fn k -> Enum.find(x, fn d -> d.key == k end).value end)
+
+    values
+    |> Enum.sum()
+  end
 end
